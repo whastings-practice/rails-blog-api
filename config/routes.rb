092e102509024753b1606c39701f5c41eef9cb5a
Will Hastings <will@objectdotcreate.net>
2017-01-15 11:26:09 -0800

    Set up home API route and catch-all route in router.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   root to: 'pages#index'
 
-  get '/*' => 'pages#index'
-
   namespace :api do
+    get 'pages/home', to: 'pages#home'
     resources :posts, only: [:index]
   end
+
+  get '*anything' => 'pages#index'
 end
